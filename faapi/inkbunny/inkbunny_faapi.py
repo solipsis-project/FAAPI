7bd979f7fc13a93fac071e81fd03a3670c6bef94
Solipsis <the.solipsis.project@gmail.com>
2023-01-29 00:52:40 -0800

    Initial implementation of InkBunny backend.

---
+++ faapi/inkbunny/inkbunny_faapi.py
@@ -0,0 +1,365 @@
+from datetime import datetime, timezone
+import functools
+from http.cookiejar import Cookie, CookieJar
+import re
+from re import sub
+from time import sleep
+from time import time
+from typing import Any, Dict, List, Tuple, Type
+from typing import Optional
+from typing import Union
+from urllib.robotparser import RobotFileParser
+from xmlrpc.client import Boolean
+from bs4 import BeautifulSoup
+from dateutil.parser import parse as parse_date
+from dateutil.tz import tzutc
+
+from urllib.parse import quote
+
+from faapi.base import FAAPI_BASE
+
+from ..connection import CloudflareScraper
+from ..connection import CookieDict
+from ..connection import Response
+from ..connection import get_robots
+from ..connection import make_session
+from ..exceptions import DisallowedPath, NonePage, ParsingError
+from ..exceptions import Unauthorized
+from ..journal import Journal, JournalStats
+from ..journal import JournalPartial
+from ..submission import Submission, SubmissionUserFolder
+from ..submission import SubmissionPartial
+from ..user import User, UserStats
+from ..user import UserPartial
+from . import inkbunny_parser
+
+def convertRating(rating: str) -> str:
+    match rating:
+        case "general":
+            return "General"
+        case "mature":
+            return "Mature"
+        case "explicit":
+            return "Explicit"
+
+def computeTypeFromExtension(extension: str, submitid: int) -> str:
+    match extension:
+        case "jpg" | "gif" | "png":
+            return "image"
+        case "swf":
+            return "flash"
+        case "txt" | "md" | "pdf":
+            return "text"
+        case "mp3":
+            return "music"
+    raise Exception(f"Unknown file extension {extension} on submission {submitid}")
+
+# The InkBunny API doesn't use cookies. Instead the session id is passed as a query param.
+# For now, we will reuse the `$0 config cookies` feature but extract the sid from the cookies.
+def getCookie(cookies: Union[list[CookieDict], CookieJar], name: str):
+    for cookie in cookies:
+        if isinstance(cookie, Cookie):
+            if cookie.name == name:
+                return cookie.value
+            else:
+                if cookie["name"] == name:
+                    return cookie["value"]
+
+# Long term: download every resolution (or make it configurable). For now, download the best resolution available
+
+THUMBNAIL_PRIORITY = [
+    "thumbnail_url_huge",
+    "thumbnail_url_large",
+    "thumbnail_url_medium",
+    "thumbnail_url_huge_noncustom",
+    "thumbnail_url_large_noncustom",
+    "thumbnail_url_medium_noncustom"
+]
+
+FILE_PRIORITY = [
+    "file_url_full",
+    "file_url_screen",
+    "file_url_preview"
+]
+
+USER_ICON_PRIORITY = [
+    "user_icon_url_large",
+    "user_icon_url_medium",
+    "user_icon_url_small",
+]
+
+def getFirst(d, keys):
+    for key in keys:
+        if key in d:
+            return d[key]
+    return None
+
+class InkBunnyFAAPI(FAAPI_BASE):
+    """
+    This class provides the methods to access and parse Fur Affinity pages and retrieve objects.
+    """
+    @staticmethod
+    def root() -> str:
+        return "https://www.inkbunny.net/"
+
+    def __init__(self, cookies: Union[list[CookieDict], CookieJar]):
+        """
+        :param cookies: The cookies for the session.
+        """
+
+        sid: str = getCookie(cookies, "sid")
+
+        self.web_session: CloudflareScraper = make_session(cookies)
+        self.api_session: CloudflareScraper = make_session(cookies=[])  # Session used for get requests
+        
+        super().__init__(
+            robots = get_robots(self.session, self.root()),  # robots.txt handler
+            timeout = None,  # Timeout for requests
+            raise_for_unauthorized = True  # Control login checks
+        )
+        
+        self.last_get: float = time() - self.crawl_delay  # Time of last get (UNIX time)
+
+    def get_json(self, path: str, *, skip_auth_check: bool = False,
+                   **params: Union[str, bytes, int, float]) -> Any:
+        """
+        Fetch a path with a GET request and parse it as JSON.
+
+        :param path: The path to fetch.
+        :param skip_page_check: Whether to skip checking the parsed page for errors.
+        :param skip_auth_check: Whether to skip checking the parsed page for login status.
+        :param params: Query parameters for the request.
+        :return: A BeautifulSoup object containing the parsed content of the request response.
+        """
+        response: Response = self.get(path, **params)
+        if not skip_auth_check and self.raise_for_unauthorized and response.status_code == 401:
+            raise Unauthorized("Not logged in")
+        if response.status_code != 401:
+            response.raise_for_status()
+
+        return response.json()
+    
+   
+    @property
+    def login_status(self) -> bool:
+        """
+        Check the login status of the given sid.
+
+        :return: True if the cookies belong to a login session, False otherwise.
+        """
+        return self.my_username() is not None
+    
+    def my_username(self) -> Optional[str]:
+        """
+        Fetch the username of the logged-in user.
+
+        :return: A string for the logged-in user, or None if the cookies are not from a login session.
+        """
+        response = self.get_parsed("/")
+
+        return inkbunny_parser.parse_logged_in_user(response)
+
+    def me(self) -> Optional[User]:
+        """
+        Fetch the information of the logged-in user.
+
+        :return: A User object for the logged-in user, or None if the cookies are not from a login session.
+        """
+        
+        username: Optional[str] = self.my_username()
+
+        return self.user(username) if username else None
+
+    def frontpage(self) -> list[SubmissionPartial]:
+        """
+        Fetch latest submissions from Weasyl's front page
+
+        :return: A list of SubmissionPartial objects
+        """
+        raise NotImplementedError
+
+    def submission(self, submission_id: int, get_file: bool = False, *, chunk_size: int = None
+                   ) -> tuple[Submission, Optional[bytes]]:
+        """
+        Fetch a submission and, optionally, its file.
+
+        :param submission_id: The ID of the submission.
+        :param get_file: Whether to download the submission file.
+        :param chunk_size: The chunk_size to be used for the download (does not override get_file).
+        :return: A Submission object and a bytes object (if the submission file is downloaded).
+        """
+        response: Any = self.get_json(f"/api_submissions.php", params = {"sid": self.sid, "submission_ids": str(submission_id), "show_description_bbcode_parsed": "yes", "show_pools": "yes" })
+
+        assert len(response["submissions"]) == 1
+
+        submission = response["submissions"][0]
+        assert submission["submission_id"] == submission_id
+
+        sub: Submission = Submission(
+            InkBunnyFAAPI,
+            Submission.Record(
+                id = submission["submission_id"],
+                title = submission["title"],
+                author = submission["username"],
+                rating = convertRating(submission["rating_name"]),
+                type = convertType(submission["type_name"]),
+                thumbnail_url = getFirst(submission, THUMBNAIL_PRIORITY),
+                author_title = "",
+                author_icon_url = getFirst(submission, USER_ICON_PRIORITY),
+                date = parse_date(submission["create_datetime"]),
+                tags = map(lambda x : x["keyword_name"], submission["keywords"]),
+                category = "",
+                species = "",
+                gender = "",
+                views = submission["views"],
+                comment_count = submission["comment_count"],
+                favorites = submission["favorites_count"],
+                description = submission["description_bbcode_parsed"],
+                footer = "",
+                mentions = [],
+                folder = "gallery" if (submission["scraps"] == "f") else "scraps",
+                user_folders = [SubmissionUserFolder(pool["name"], f"{self.root()}/poolview_process.php?pool_id={pool["pool_id"]}", "") for pool in submission["pools"]],
+                file_url = getFirst(submission, USER_ICON_PRIORITY),
+                prev = 0,
+                next = 0,
+                favorite = submission["favorite"],
+                favorite_toggle_link = "",
+            )
+        )
+                
+        sub_file: Optional[bytes] = self.submission_file(sub, chunk_size=chunk_size) if get_file and sub.id else None
+        return sub, sub_file
+
+    def journal(self, journal_id: int) -> Journal:
+        """
+        Fetch a journal.
+
+        :param journal_id: The ID of the journal.
+        :return: A Journal object.
+        """
+        raise NotImplementedError
+
+    def user(self, user: str) -> User:
+        """
+        Fetch a user.
+
+        :param user: The name of the user (_ characters are allowed).
+        :return: A User object.
+        """
+        beautifulSoup = self.get_parsed(quote(inkbunny_parser.username_url(user)))
+
+        return User(InkBunnyFAAPI, inkbunny_parser.parse_user_profile(beautifulSoup))
+
+    def search(self, params: Dict[str, str], page: Optional[Tuple[str, int]]):
+        if page != None:
+            return self.get_json(f"/api_search.php", params = { "sid": self.sid, "rid": page[0], "page": page[1] })
+        else:
+            return self.get_json(f"/api_search.php", params = params | { "sid": self.sid, "get_rid": "yes", "submissions_per_page": "100"})
+
+    def gallery(self, user: str, page: Optional[Tuple[str, int]] = None) -> tuple[list[SubmissionPartial], Optional[Any], list[Any]]:
+        """
+        Fetch a user's gallery page.
+
+        :param user: The name of the user (_ characters are allowed).
+        :param page: The page to fetch.
+        :return: A list of SubmissionPartial objects and the next page (None if it is the last).
+        """
+        response = self.search({"username": user, "scraps": "no"}, page)
+
+        return self.parse_folder(response)
+
+    def scraps(self, user: str, page: Optional[Tuple[str, int]] = None) -> tuple[list[SubmissionPartial], Optional[Any], list[Any]]:
+        """
+        Fetch a user's scraps page.
+
+        :param user: The name of the user (_ characters are allowed).
+        :param page: The page to fetch.
+        :return: A list of SubmissionPartial objects and the next page (None if it is the last).
+        """
+        response = self.search({"username": user, "scraps": "yes"}, page)
+
+        return self.parse_folder(response)
+      
+            
+    def parse_folder(response: Any, user: str):
+        submissions = [SubmissionPartial(InkBunnyFAAPI, SubmissionPartial.Record(
+            id = s["submission_id"],
+            title = s["title"],
+            rating = convertRating(s["rating_name"]),
+            type = computeTypeFromExtension(s["type_name"]),
+            thumbnail_url = getFirst(s, THUMBNAIL_PRIORITY),
+            author = s["username"]
+        )) for s in response["submissions"]]
+
+        if response["page"] == response["pages_count"]:
+            return (submissions, None, [])
+        
+        next_page_number: int = int(response["page"]) + 1
+        
+        return (submissions, (response["rid"], next_page_number), [])
+
+
+    def favorites(self, user: str, page: Any = None) -> tuple[list[SubmissionPartial], Optional[Any], list[Any]]:
+        """
+        Fetch a user's favorites page.
+
+        :param user: The name of the user (_ characters are allowed).
+        :param page: The page to fetch.
+        :return: A list of SubmissionPartial objects and the next page (None if it is the last).
+        """
+        raise NotImplementedError
+
+    def journals(self, user: str, page: Any = 1) -> tuple[list[JournalPartial], Optional[Any], list[Any]]:
+        """
+        Fetch a user's journals page.
+
+        :param user: The name of the user (_ characters are allowed).
+        :param page: The page to fetch.
+        :return: A list of Journal objects and the next page (None if it is the last).
+        """
+        raise NotImplementedError
+        
+    def watchlist_to(self, user: str, page: Any = 1) -> tuple[list[UserPartial], Optional[Any], list[Any]]:
+        """
+        Fetch a page from the list of users watching the user.
+
+        :param user: The name of the user (_ characters are allowed).
+        :param page: The page to fetch.
+        :return: A list of UserPartial objects and the next page (None if it is the last).
+        """
+        raise NotImplementedError
+
+    def watchlist_by(self, user: str, page: Any = 1) -> tuple[list[UserPartial], Optional[Any], list[Any]]:
+        """
+        Fetch a page from the list of users watched by the user.
+        :param user: The name of the user (_ characters are allowed).
+        :param page: The page to fetch.
+        :return: A list of UserPartial objects and the next page (None if it is the last).
+        """
+        if user != self.my_username():
+            raise NotImplementedError
+
+        response = self.get_json(f"/api_watchlist.php", params = { "sid": self.sid })
+        
+        followers = [UserPartial(InkBunnyFAAPI, UserPartial.Record(
+            name = watch["username"],
+            status = "",
+            title = "",
+            join_date = datetime.fromtimestamp(0, tz = tzutc()),
+            user_icon_url = ""
+        )) for watch in response["watches"]]
+
+        return (followers, None, [])
+
+    def check_page_raise(self, page: BeautifulSoup) -> None:
+        # Weasyl returns a non-200 status code on failure.
+        if page is None:
+            raise NonePage
+
+    @staticmethod
+    def html_to_bbcode(html: str) -> str:
+        raise NotImplementedError
+
+    @staticmethod
+    def username_url(username: str) -> str:
+        return sub(r"[^a-z\d.~`-]", "", username.lower())
---
+++ faapi/inkbunny/inkbunny_parser.py
@@ -0,0 +1,155 @@
+from datetime import datetime, timezone
+from re import Pattern
+from re import compile as re_compile
+from re import match
+from re import search
+from re import sub
+import re
+from typing import Any, Dict
+from typing import Optional
+from typing import NewType
+from dateutil.tz import tzutc
+
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString
+from bs4.element import Tag
+from dateutil.parser import parse as parse_date
+
+from faapi.parse import clean_html, inner_html
+from faapi.user import User, UserStats
+
+from ..exceptions import DisabledAccount, Unauthorized
+from ..exceptions import NoTitle
+from ..exceptions import NonePage
+from ..exceptions import NotFound
+from ..exceptions import NoticeMessage
+from ..exceptions import ParsingError
+from ..exceptions import ServerError
+from ..exceptions import _raise_exception
+
+from abc import ABC, abstractmethod
+from datetime import datetime
+from re import MULTILINE
+from re import Match
+from re import Pattern
+from re import compile as re_compile
+from re import match
+from re import search
+from re import sub
+from typing import Any
+from typing import Optional
+from typing import Union
+
+from bbcode import Parser as BBCodeParser  # type:ignore
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString
+from bs4.element import Tag
+from dateutil.parser import parse as parse_date
+from htmlmin import minify  # type:ignore
+
+from ..exceptions import DisabledAccount
+from ..exceptions import NoTitle
+from ..exceptions import NonePage
+from ..exceptions import NotFound
+from ..exceptions import NoticeMessage
+from ..exceptions import ParsingError
+from ..exceptions import ServerError
+from ..exceptions import _raise_exception
+
+def parse_logged_in_user(bs: BeautifulSoup) -> Optional[str]:
+    username_tag = bs.select_one("#usernavigation .loggedin_userdetails a.widget_userNameSmall")
+    if not username_tag:
+        return None
+
+    return username_tag.text
+
+def find_title_tag(bs: BeautifulSoup, title: str):
+    for title_tag in bs.select("div.title"):
+        if title_tag.text == title:
+            return title_tag.next_sibling.span
+    _raise_exception(ParsingError(f"Missing {title} tag"))
+
+def parse_contact_details(bs: BeautifulSoup) -> dict[str, str]:
+    contacts_tag = find_title_tag(bs, "Links and Contact Details")
+
+    contacts: dict[str, str] = {}
+
+    for div_tag in contacts_tag.next_siblings:
+        children = div_tag.find_all("div", recursive = False)
+        if len(children) == 2:
+            site_name = children[0].text
+            link = children[1].a["href"]
+            contacts[site_name] = link
+
+    return contacts
+
+def parse_user_profile(username: str, bs: BeautifulSoup) -> User.Record:
+    
+
+
+    profile_tag = find_title_tag("Profile")
+    profile_description = clean_html(inner_html(profile_tag))
+
+    views_tag = bs.select_one('span[title="Submission Views Received"] > strong')
+    assert views_tag is not None, _raise_exception(ParsingError("Missing views tag"))
+
+    submissions_tag = bs.select_one('span[title="Submissions Uploaded"] > strong')
+    assert submissions_tag is not None, _raise_exception(ParsingError("Missing submissions tag"))
+
+    favorites_tag = bs.select_one('span[title="Favorites Received"] > strong')
+    assert favorites_tag is not None, _raise_exception(ParsingError("Missing favorites tag"))
+
+    comments_earned_tag = bs.select_one('span[title="Comments Received"] > strong')
+    assert comments_earned_tag is not None, _raise_exception(ParsingError("Missing comments received tag"))
+
+    comments_made_tag = bs.select_one('span[title="Comments Given"] > strong')
+    assert comments_made_tag is not None, _raise_exception(ParsingError("Missing comments given tag"))
+
+    journals_tag = bs.select_one('span[title="Journals Created"] > strong')
+    assert journals_tag is not None, _raise_exception(ParsingError("Missing journals tag"))
+
+    watchers_tag = bs.select_one('span[title="Watches Received"] > strong')
+    assert watchers_tag is not None, _raise_exception(ParsingError("Missing watchers tag"))
+
+    watches_tag = bs.select_one('#watches strong')
+    assert watches_tag is not None, _raise_exception(ParsingError("Missing watches tag"))
+
+    contacts = parse_contact_details(bs)    
+
+    profile_image_tag = bs.select_one('meta[property="og:image"]')
+    assert profile_image_tag is not None, _raise_exception(ParsingError("Missing profile image tag"))
+
+    watchbox_tag = bs.select_one("#widget-watchbox-watchstate")
+    assert watchbox_tag is not None, _raise_exception(ParsingError("Missing profile watchbox tag"))
+
+    watched = (watchbox_tag["value"] == "true")
+
+    block_remove_form = bs.select_one("#block_remove_form")
+    assert block_remove_form is not None, _raise_exception(ParsingError("Missing block remove form tag"))
+
+    blocked = "UnBlock user's submissions." in block_remove_form.next_sibling.next_sibling.text
+
+    return User.Record(
+        name = username,
+        status = "",
+        profile = profile_description,
+        title = "",
+        join_date = datetime.fromtimestamp(0),
+        stats = UserStats(
+            views= int(views_tag.text),
+            submissions= int(submissions_tag.text),
+            favorites= int(favorites_tag.text),
+            comments_earned= int(comments_earned_tag.text),
+            comments_made= int(comments_made_tag.text),
+            journals= int(journals_tag.text),
+            watched_by= int(watchers_tag.text),
+            watching= int(watches_tag.text)
+        ),
+        info = {},
+        contacts= contacts,
+        user_icon_url = profile_image_tag["content"],
+        watched= watched,
+        watched_toggle_link= None,
+        blocked= blocked,
+        blocked_toggle_link= None
+    )
